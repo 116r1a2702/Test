1110add8587981a607f3627f55f9e3d1e2a4b9b6
chaitanya <bsrikrishnachaitanya@gmail.com>
2021-02-22 21:11:53 +0530

    sysout written

--- Git Test Project/src/com/git/test/GitTestExample.java
+++ Git Test Project/src/com/git/test/GitTestExample.java
@@ -1,7 +1,9 @@
 package com.git.test;
 
 public class GitTestExample {
-public static void main(String[] args) {
 	
+	
+public static void main(String[] args) {
+	System.out.println("test 2");
 }
 }
